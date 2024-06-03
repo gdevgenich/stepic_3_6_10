a2fe0a279305b1935dd1074ca54fe2759911a9d2
Dmitrii <gdevgenich@gmail.com>
2024-06-03 12:49:51 +0400

    Fix error message text

--- test_items.py
+++ test_items.py
@@ -13,4 +13,4 @@ def test_add_to_card_button(browser, user_language):
     cart_button_text = WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR,
                                                                                           "#add_to_basket_form button"))).text
     assert cart_button_text == add_to_cart_text.get(user_language), \
-        "Incorrect text on Cart button expected {user_language} but got {cart_button_text}"
+        f"Incorrect text on Cart button expected {user_language} but got {cart_button_text}"
